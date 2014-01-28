b45251d9e65bec9eaa693402297a980c22dcf84f
shalini-ramachandra <Shalini.Ramachandra@happiestminds.com>
2014-01-28 12:47:57 +0530

    Updated to download to local folder from Azure Storage

--- DownloadBuildBinariesFromAzureStorage.ps1
+++ DownloadBuildBinariesFromAzureStorage.ps1
@@ -1,7 +1,6 @@
 <#
 .SYNOPSIS
-    Copies Build binaries from Azure Storage Container to a local folder on the
-	Azure Windows VM.
+    Copies Build binaries from Azure Storage Container to a local folder.
 
 .DESCRIPTION
     Copies blobs from a single storage container to a local directory.  If the blobs 
@@ -9,24 +8,11 @@
 	recreate that directory hierarchy under the local destination path specified.
 
 .EXAMPLE
-    .\DownloadBuildBinariesFromAzureStorage -VMDNS 'vm-dns.cloudapp.net' `
-        -UserName 'myUserName' -Password 'myPassword' `
+    .\DownloadBuildBinariesFromAzureStorage `
 		-StorageAccountName 'myStorageAccountName' -StorageAccountKey myStorageAccountKey `
 		-ContainerName 'myContainerName' -Destination 'myLocalPath'
 #>
 param (
-	# The DNS of the Azure Cloud Service where the Windows VM is deployed
-    [Parameter(Mandatory = $true)]
-	$VMDNS,
-	
-    # The User Name to connect to the VM
-    [Parameter(Mandatory = $true)]
-	$UserName,
-	
-    # The Password to connect to the VM
-    [Parameter(Mandatory = $true)]
-	$Password,
-	
 	# The Azure Storage Account Name where the Build Binaries exist
     [Parameter(Mandatory = $true)]
 	$StorageAccountName,
@@ -45,20 +31,10 @@ param (
 )
 
 
-# Create the Remote Session
-$securePassword = ConvertTo-SecureString -AsPlainText -Force -String $Password
-$credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $UserName, $securePassword
-$session  = New-PSSession -ComputerName $VMDNS -Credential $credential
+# Create the Azure Storage Context using the Account Name and Account Key
+$context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
 
-$downloadBuildOutput = {
-	param($StorageAccountName, $StorageAccountKey, $ContainerName, $Destination)
+# List the Azure Blobs in a given container and download each of the listed blobs to the destination folder
+Get-AzureStorageBlob -container $ContainerName -context $context | ForEach-Object {Get-AzureStorageBlobContent -container $ContainerName -context $context -blob $_.Name -Force -Destination $Destination}
 
-	$context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
 
-	Get-AzureStorageBlob -container $ContainerName -context $context | ForEach-Object {Get-AzureStorageBlobContent -container $ContainerName -context $context -blob $_.Name -Force -Destination $Destination}
-}
-
-Invoke-Command -Session $session $downloadBuildOutput -ArgumentList  $StorageAccountName, $StorageAccountKey, $ContainerName, $Destination
-
-# Remove the remote session
-Remove-PSSession -Session $session
