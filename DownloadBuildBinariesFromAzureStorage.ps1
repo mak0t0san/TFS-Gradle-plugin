17ea0fb03c9d6be1ad01a86532b55abfa77affbc
shalini-ramachandra <Shalini.Ramachandra@happiestminds.com>
2014-02-12 20:59:37 +0530

    Work-in-progress: Installing Azure PowerShell if not already present on the VM, before downloading

--- DownloadBuildBinariesFromAzureStorage.ps1
+++ DownloadBuildBinariesFromAzureStorage.ps1
@@ -37,83 +37,141 @@ param (
 )
 
 
-Try
+function logAzureError ($errorObj)
 {
-	# Reset the error variable
-	$error.clear()
-
-    $cmdletError = ""
-	
-	# Set the properties for logging the status of download
-	$logFile = "C:\DownloadBuildBinariesFromAzureStorage.log" 
-	$logFileContent =  "================================================================================================================================`n" `
-					 + "                                 DOWNLOAD STATUS FOR BUILD - `"" + $BlobNamePrefix + "`"                                        `n" `
-					 + "================================================================================================================================`n"
- 
-    function logAzureError ($errorObj)
-    {
 	    $errorMsg = $errorObj.InvocationInfo.InvocationName.ToString() + "  :  " + $errorObj.ToString() + "`n" `
 					    + $errorObj.InvocationInfo.PositionMessage.ToString() + "`n" `
 					    + "CategoryInfo  :  " + $errorObj.CategoryInfo.ToString() + "`n" `
 					    + "FullyQualifiedErrorId  :  " + $errorObj.FullyQualifiedErrorId.ToString()
 	
 	    return $errorMsg
-    }
-
-
-	# Create the Azure Storage Context using the Account Name and Account Key
-	$context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-	$cmdletError = ""
+}
 	
-    if ($error.Count -gt 0)
+Try
+{
+	# Reset the error variable
+	$error.clear()
+
+    $cmdletError = ""	
+	$continueScriipt = $false
+	
+	# Set the properties for logging the status of download
+	$logFile = "C:\DownloadBuildBinariesFromAzureStorage.log" 
+	$logFileContent =  "================================================================================================================================`n" `
+					 + "                                 DOWNLOAD STATUS FOR BUILD - `"" + $BlobNamePrefix + "`"                                        `n" `
+					 + "================================================================================================================================`n"
+
+	# Check if Azure PowerShell module exists, before installing it
+	$azureCmdlet = Get-Module -Name Azure
+	if ($azureCmdlet.Name -eq "Azure")
 	{
-		$cmdletError = logAzureError $error[0]
-		$logFileContent = $logFileContent + "`n" + $cmdletError
+		$logFileContent = $logFileContent + "Windows Azure PowerShell is already installed. `n"
+		$continueScript = $true
 	}
 	else
 	{
-        # List the Azure Blobs matching the BlobNamePrefix
-		$blobsList = Get-AzureStorageBlob -Container $ContainerName -Context $context -Prefix $BlobNamePrefix
-        $cmdletError = ""
+		$logFileContent = $logFileContent + "Windows Azure PowerShell is not installed. `n"
+		
+		# Check if Chocolatey command exists, before installing it
+		Get-Command 'cinst'
+		if ($error.Count -gt 0)
+		{
+			# Reset the error variable
+ 	        $error.clear()
+					
+			$logFileContent = $logFileContent + "Chocolatey does not exist. Installation will start.... `n"
+			
+			$origExecPolicy = Get-ExecutionPolicy
+			$logFileContent = $origExecPolicy
+			Set-ExecutionPolicy 'Unrestricted'
+			
+			# Install Chocolatey
+			iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+			
+			Set-ExecutionPolicy $origExecPolicy	
 
+			# Update the environment variable PATH
+			$systemDrive = iex ('$env:systemdrive')
+			$pathEnv = iex ('$env:path')
+			$pathEnv = $pathEnv + ';' + $systemDrive + '\chocolatey\bin'
+			iex ('$env:path = "' + $pathEnv + '"')				
+			
+			$logFileContent = $logFileContent + "....Chocolatey has been successfully installed. `n"
+		}
+		else
+		{
+			$logFileContent = $logFileContent + "Chocolatey is already installed. `n"
+		}
+
+		$logFileContent = $logFileContent + "Windows Azure PowerShell installation will start..... `n"
+		
+		# Install WindowsAzurePowerShell
+		iex ('cinst WindowsAzurePowershell')
+		
+		$logFileContent = $logFileContent + "....Windows Azure PowerShell has been successfully installed. `n"
+		
+		$continueScript = $true
+	}	
+	
+	if ($continueScript)
+	{
+		# Create the Azure Storage Context using the Account Name and Account Key
+		$context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+		$cmdletError = ""
+		
 		if ($error.Count -gt 0)
 		{
 			$cmdletError = logAzureError $error[0]
-            $logFileContent = $logFileContent + "`n" + $cmdletError
+			$logFileContent = $logFileContent + "`n" + $cmdletError
 		}
-		else 
+		else
 		{
-			# Download each of the listed blobs to the destination folder
-			foreach($blob in $blobsList)
+			# List the Azure Blobs matching the BlobNamePrefix
+			$blobsList = Get-AzureStorageBlob -Container $ContainerName -Context $context -Prefix $BlobNamePrefix
+			$cmdletError = ""
+
+			if ($error.Count -gt 0)
 			{
-				
-		        $downloadResult = Get-AzureStorageBlobContent -Container $ContainerName -Context $context -Blob $blob.Name -Destination $Destination -Force
-		        $cmdletError = ""
+				$cmdletError = logAzureError $error[0]
+				$logFileContent = $logFileContent + "`n" + $cmdletError
+			}
+			else 
+			{
+				# Download each of the listed blobs to the destination folder
+				foreach($blob in $blobsList)
+				{
+					
+					$downloadResult = Get-AzureStorageBlobContent -Container $ContainerName -Context $context -Blob $blob.Name -Destination $Destination -Force
+					$cmdletError = ""
 
-		        if ($error.Count -gt 0)	
-		        {
-			        $cmdletError = logAzureError $error[0]
+					if ($error.Count -gt 0)	
+					{
+						$cmdletError = logAzureError $error[0]
 
-			        $logFileContent = $logFileContent + "`n" `
-								        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FAILURE START~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n" `
-								        + $cmdletError + "`n" `
-								        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FAILURE END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n" 
-		        }
-		        else
-		        {
-			        $logFileContent = $logFileContent + "`n" `
-								        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SUCCESS START~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n" `
-								        + "AbsoluteUri  : " + $downloadResult.ICloudBlob.Uri.AbsoluteUri + "`n" `
-								        + "Blob Name    : " + $downloadResult.Name + "`n" `
-								        + "Blob Type    : " + $downloadResult.BlobType + "`n" `
-								        + "Length       : " + $downloadResult.Length + "`n" `
-								        + "ContentType  : " + $downloadResult.ContentType + "`n" `
-								        + "LastModified : " + $downloadResult.LastModified.UtcDateTime + "`n" `
-								        + "SnapshotTime : " + $downloadResult.SnapshotTime + "`n" `
-								        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SUCCESS END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n"
-		        }
-            }
-        }
+						$logFileContent = $logFileContent + "`n" `
+											+ "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FAILURE START~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n" `
+											+ $cmdletError + "`n" `
+											+ "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FAILURE END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n" 
+						
+						# Reset the error variable, else for all subsequent downloads, the previous error will be considered
+						$error.clear()
+					}
+					else
+					{
+						$logFileContent = $logFileContent + "`n" `
+											+ "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SUCCESS START~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n" `
+											+ "AbsoluteUri  : " + $downloadResult.ICloudBlob.Uri.AbsoluteUri + "`n" `
+											+ "Blob Name    : " + $downloadResult.Name + "`n" `
+											+ "Blob Type    : " + $downloadResult.BlobType + "`n" `
+											+ "Length       : " + $downloadResult.Length + "`n" `
+											+ "ContentType  : " + $downloadResult.ContentType + "`n" `
+											+ "LastModified : " + $downloadResult.LastModified.UtcDateTime + "`n" `
+											+ "SnapshotTime : " + $downloadResult.SnapshotTime + "`n" `
+											+ "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SUCCESS END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n"
+					}
+				}
+			}
+		}
 	}
 }
 Catch
