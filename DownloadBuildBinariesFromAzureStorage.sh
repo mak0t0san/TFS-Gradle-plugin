6121a190322700591bc2ed49e9db573f9bdfa3a7
shalini-ramachandra <Shalini.Ramachandra@happiestminds.com>
2014-02-12 14:28:04 +0530

    Downloading blobs based on build number format in blob name. closes #5

--- DownloadBuildBinariesFromAzureStorage.sh
+++ DownloadBuildBinariesFromAzureStorage.sh
@@ -4,23 +4,24 @@
 originalIFS=$IFS
 
 # Validate the number of input arguments
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
-    echo "Usage: $0 <storageAccountName> <storageAccountKey> <containerName> <destinationPath>"
+    echo "Usage: $0 <storageAccountName> <storageAccountKey> <containerName> <blobNamePrefix> <destinationPath>"
     exit 2
 fi
 
 storageAccountName=$1
 storageAccountKey=$2
 containerName=$3
-destination=$4
+blobNamePrefix=$4
+destination=$5
 
 
 # Change the IFS to newline character (\n) so that each output line while listing blobs can be stored into an array
 IFS=$'\n'
 
 # List all the blobs in the container and get the name of the blobs into an array
-blobNames=(`azure storage blob list --account-name $storageAccountName --account-key $storageAccountKey --container $containerName --json | grep '"name":' | awk -F'"' '{print $4}'`)
+blobNames=(`azure storage blob list --account-name $storageAccountName --account-key $storageAccountKey --container $containerName --prefix "$blobNamePrefix" --json | grep '"name":' | awk -F'"' '{print $4}'`)
 
 # Restore the original TFS
 IFS=$originalIFS
@@ -33,15 +34,19 @@ IFS="\`"
 # Get the destination's final directory name and path separately. Else there will be a problem specifying
 # a directory path starting with / in the "azure storage blob download --destination" argument
 destinationDir=(`echo "$destination" | awk -F "/" '{print $NF}'`)
-destinationPath=(`echo "$destination" | awk -F "/$destinationDir" '{print $1}'`)
+destinationPath=(`echo "$destination" | awk -F "/$destinationDir" '{if (NF > 1) print $1}'`)
 
 # Restore the original TFS
 IFS=$originalIFS
 
 # Change directory to the destination directory
-cd $destinationPath
+if [ ! -z "$destinationPath" ]
+then
+	cd $destinationPath
+fi
 
-# Download each blob into the destination path. This will replace any files / folders with the same name, which may be already present in the destination path
+# Download each blob into the destination path. This will replace any files / folders with the same name, 
+# which may be already present in the destination path
 for blob in "${blobNames[@]}"
 do
     # Blob name and Destination Path should be enclosed in quotes as spaces may exist in the name
