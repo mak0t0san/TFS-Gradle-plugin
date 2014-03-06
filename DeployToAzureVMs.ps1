3a3dfffdee4d5188d0deafd1cc7558d423e1975d
shalini-ramachandra <Shalini.Ramachandra@happiestminds.com>
2014-03-06 18:24:08 +0530

    Added logic for Windows VM

--- DeployToAzureVMs.ps1
+++ DeployToAzureVMs.ps1
@@ -17,7 +17,7 @@
         -SubscriptionId "<myAzureSubscriptionId>" -AzureManagementCertificate "<managementCertificatePath>" `
         -CloudServiceName <myCloudServiceName> `
         -StorageAccountName <myStorageAccountName> -StorageAccountKey <myStorageAccountKey> `
-        -ContainerName <myContainerName> -BlobNamePrefix "<blobNamePrefix>" `
+        -StorageContainerName <myContainerName> -BlobNamePrefix "<blobNamePrefix>" `
         -VMUserName <vmUserName> `
 		-WinPassword <winPassword> -WinCertificate "<winCertificatePath>" -WinAppPath "<winAppPath>" `
         -LinuxSSHKey "<linuxSSHKey>" -LinuxAppPath "<linuxAppPath>"
@@ -45,7 +45,7 @@ param (
 	
 	# The Azure Storage Container Name
     [Parameter(Mandatory = $true)]
-    $ContainerName,
+    $StorageContainerName,
 
     # The Blob Name Prefix of the blobs to be downloaded
     [Parameter(Mandatory = $true)]
@@ -76,25 +76,108 @@ param (
     $LinuxAppPath
 )
 
+$WinPassword >> 'logFile.txt'
+
+# Import the Certificate to the Local Machine Trusted Store
+function ImportCertificate($certToImport)
+{
+    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store "Root", "LocalMachine"
+    $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+    $store.Add($CertToImport)
+    $store.Close()
+
+    <#
+    $certCmd = 'CERTUTIL -addstore -enterprise -f -v root "' + $AzureManagementCertificate + '"'
+    Invoke-Command -ScriptBlock {CERTUTIL -addstore -enterprise -f -v root "$AzureManagementCertificate"}
+    #>
+}
+
 
 
 # Import the Azure Management Certificate
-$CertToImport = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $AzureManagementCertificate
-$mgmtCertThumbprint = $CertToImport.Thumbprint
-$store = New-Object System.Security.Cryptography.X509Certificates.X509Store "Root", "LocalMachine"
-$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-$store.Add($CertToImport)
-$store.Close()
+$certToImport = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $AzureManagementCertificate
+# ImportCertificate $certToImport
 
 
-# Use the Get Cloud Service Properties Service Management REST API to get the details of all the
-# VMs hosted in the Cloud Service
+$mgmtCertThumbprint = $certToImport.Thumbprint
+$cloudServieDNS = $CloudServiceName + ".cloudapp.net"
+
+
+# Use the 'Get Cloud Service Properties' Service Management REST API to get the details of 
+# all the VMs hosted in the Cloud Service
 $reqHeaderDict = @{}
 $reqHeaderDict.Add('x-ms-version','2012-03-01') # API version
 $restURI = "https://management.core.windows.net/" + $SubscriptionId + "/services/hostedservices/" + $CloudServiceName + "?embed-detail=true"
+[xml]$cloudProperties = Invoke-RestMethod -Uri $restURI -CertificateThumbprint $mgmtCertThumbprint -Headers $reqHeaderDict 
 
-[xml]$cloudPropXML = Invoke-RestMethod -Uri $restURI -CertificateThumbprint $mgmtCertThumbprint -Headers $reqHeaderDict 
 
-# TEMP CODE
-Write-Host $cloudPropXML.HostedService.ServiceName
+# Iterate through the Cloud Properties and get the details of each VM.
+# Depending on the OS (Windows or Linux), execute corresponding download scripts.
+$cloudProperties.HostedService.Deployments.Deployment.RoleList.Role | foreach {
+    
+    $OS = $_.OSVirtualHardDisk.OS
 
+    if ($OS -ieq "Windows")
+    {
+
+        $publicWinRMPort = "0"
+
+        $_.ConfigurationSets.ConfigurationSet.InputEndpoints.InputEndpoint | foreach {
+            if ($_.LocalPort -eq "5986")
+            {
+                $publicWinRMPort = $_.Port                
+            }
+        }
+
+        Write-Host "validating port...."
+
+        if ($publicWinRMPort -eq "0")
+        {
+            # TODO: THROW ERROR - WinRM HTTPS Public Port not defined
+        }
+        else
+        {
+            Write-Host "validating port....done"
+
+            # Import the VM Certificate
+            $certToImport = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $WinCertificate
+            # ImportCertificate $certToImport
+
+            Write-Host "Triggering download script...."
+            $securePassword = ConvertTo-SecureString $WinPassword
+            $credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $VMUserName, $securePassword
+            $sessionOption = New-PSSessionOption -SkipCACheck
+            Invoke-Command -ComputerName $cloudServieDNS -InDisconnectedSession `
+                -Credential $credential -UseSSL -SessionOption $sessionOption `
+                -FilePath DownloadBuildBinariesFromAzureStorage.ps1 `
+                -ArgumentList $StorageAccountName, $StorageAccountKey, $StorageContainerName, $WinAppPath, $BlobNamePrefix
+        }
+    }
+    elseif ($OS -ieq "Linux")
+    {
+        Write-Host $OS
+
+        $publicSSHPort = 0
+
+        $_.Role.ConfigurationSets.ConfigurationSet.InputEndpoints | ForEach-Object {
+            if ($_.InputEndpoint.LocalPort -eq 22)
+            {
+                $publicSSHPort = $_.InputEndpoint.Port
+                break
+            }
+        }
+
+        if ($publicSSHPort -eq 0)
+        {
+            # TODO: THROW ERROR - SSH Public Port not defined
+        }
+        else
+        {
+            
+        }
+    }
+    else
+    {
+        # TODO: THROW ERROR - OS not supported
+    }
+}
