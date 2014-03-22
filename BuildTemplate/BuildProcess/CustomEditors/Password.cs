fbe7dfb04745dcc3b97f9bfc46c1d1a45b2e2b7b
Thirugnanam <thirugnanam@gmail.com>
2014-03-22 16:15:07 +0530

    Added Build template project and PSI files
    Added Build template project and PSI files

---
+++ BuildTemplate/BuildProcess/CustomEditors/Password.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.Client;
+
+namespace BuildProcess.CustomEditors
+{
+    [BuildExtension(HostEnvironmentOption.All)]
+    public class Password
+    {
+        public string PasswordField { get; set; }
+        
+        public override string ToString()
+        {
+            string maskedPassword = "";
+
+            for (int i = 0; i < PasswordField.Length; i++)
+            {
+                maskedPassword += "*";
+            }
+
+            //return maskedPassword;
+            return PasswordField;
+        }
+    }
+}
